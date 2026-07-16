c92c8cae4fd1a10139c8260736a6ce5e4ecff165
Rakesh Kale <kalerakesh4030@gmail.com>
2026-07-16 17:18:37 +0530

    Fix build issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import  {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
-import Contact from './pages/contact'
+import Contact from './pages/Contact'
 import About from './pages/About'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
